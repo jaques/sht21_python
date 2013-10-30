cdc9f0aa374bc9c8c7642fa5c2165ced6c704e5b
Richard Jaques <richard.p.jaques@gmail.com>
2013-10-30 23:27:36 +0000

    Added some basic unit (sanity) tests

--- sht21.py
+++ sht21.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import fcntl
 import time
+import unittest
 
 class SHT21:
     """Class to read temperature and humidity from SHT21, much of class was 
@@ -34,8 +35,8 @@ class SHT21:
         self.i2c.write(chr(self._TRIGGER_TEMPERATURE_NO_HOLD))
         time.sleep(0.250)
         data = self.i2c.read(3)
-        if self._calculate_checksum(data,2) == ord(data[2]):
-            return self._get_temperature_from_buffer(data)
+        if _calculate_checksum(data,2) == ord(data[2]):
+            return _get_temperature_from_buffer(data)
         
 
     def read_humidity(self):    
@@ -44,8 +45,8 @@ class SHT21:
         self.i2c.write(chr(self._TRIGGER_HUMIDITY_NO_HOLD))
         time.sleep(0.250)
         data = self.i2c.read(3)
-        if self._calculate_checksum(data,2) == ord(data[2]):
-            return self._get_humidity_from_buffer(data)    
+        if _calculate_checksum(data,2) == ord(data[2]):
+            return _get_humidity_from_buffer(data)    
         
 
     def close(self):
@@ -63,47 +64,62 @@ class SHT21:
         self.close()
 
         
-    def _calculate_checksum(self, data, nbrOfBytes):
-        """5.7 CRC Checksum using teh polynomial given in the datasheet"""
-        # CRC
-        POLYNOMIAL = 0x131 # //P(x)=x^8+x^5+x^4+1 = 100110001
-        crc = 0
-        #calculates 8-Bit checksum with given polynomial
-        for byteCtr in range(nbrOfBytes):
-            crc ^= (ord(data[byteCtr]))
-            for bit in range(8,0,-1):
-                if (crc & 0x80):
-                    crc = (crc << 1) ^ POLYNOMIAL
-                else:
-                    crc = (crc << 1)
-        return crc
+def _calculate_checksum(data, nbrOfBytes):
+    """5.7 CRC Checksum using teh polynomial given in the datasheet"""
+    # CRC
+    POLYNOMIAL = 0x131 # //P(x)=x^8+x^5+x^4+1 = 100110001
+    crc = 0
+    #calculates 8-Bit checksum with given polynomial
+    for byteCtr in range(nbrOfBytes):
+        crc ^= (ord(data[byteCtr]))
+        for bit in range(8,0,-1):
+            if (crc & 0x80):
+                crc = (crc << 1) ^ POLYNOMIAL
+            else:
+                crc = (crc << 1)
+    return crc
 
 
-    def _get_temperature_from_buffer(self, data):
-        """This function reads the first two bytes of data and 
-	returns the temperature in C by using the following function:
-        T = =46.82 + (172.72 * (ST/2^16))
-        where ST is the value from the sensor
-        """
-        unadjusted = (ord(data[0]) << 8) + ord(data[1])
-        unadjusted *= 175.72
-        unadjusted /= 1 << 16 # divide by 2^16
-        unadjusted -= 46.85
-        return unadjusted
+def _get_temperature_from_buffer(data):
+    """This function reads the first two bytes of data and 
+    returns the temperature in C by using the following function:
+    T = =46.82 + (172.72 * (ST/2^16))
+    where ST is the value from the sensor
+    """
+    unadjusted = (ord(data[0]) << 8) + ord(data[1])
+    unadjusted *= 175.72
+    unadjusted /= 1 << 16 # divide by 2^16
+    unadjusted -= 46.85
+    return unadjusted
 
 
-    def _get_humidity_from_buffer(self, data):
-        """This function reads the first two bytes of data and returns 
-	the relative humidity in percent by using the following function:
-        RH = -6 + (125 * (SRH / 2 ^16))
-        where SRH is the value read from the sensor
-        """
-        unadjusted = (ord(data[0]) << 8) + ord(data[1])
-        unadjusted *= 125
-        unadjusted /= 1 << 16 # divide by 2^16
-        unadjusted -= 6
-        return unadjusted
+def _get_humidity_from_buffer(data):
+    """This function reads the first two bytes of data and returns 
+    the relative humidity in percent by using the following function:
+    RH = -6 + (125 * (SRH / 2 ^16))
+    where SRH is the value read from the sensor
+    """
+    unadjusted = (ord(data[0]) << 8) + ord(data[1])
+    unadjusted *= 125
+    unadjusted /= 1 << 16 # divide by 2^16
+    unadjusted -= 6
+    return unadjusted
+    
 
+class SHT21Test(unittest.TestCase):
+    """simple sanity test.  Run from the command line with 
+    python -m unittest sht21 to check they are still good"""
+
+    def test_temperature(self):
+        """Unit test to check the checksum method"""
+        calc_temp = _get_temperature_from_buffer([chr(99),chr(172)])
+        #floating point comparison, not pretty.
+        self.failUnless(abs(calc_temp - 21.5653979492) < 0.1)
+
+    def test_checksum(self):
+        """Unit test to check the checksum method.  Uses values read"""
+        self.failUnless(_calculate_checksum([chr(99),chr(172)] , 2) == 249)
+        self.failUnless(_calculate_checksum([chr(99),chr(160)] , 2) == 132)
 
 if __name__ == "__main__":
     try:
